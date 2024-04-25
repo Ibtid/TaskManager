23e3a7e9402e8aa7b1b37968ba0ddc2e9bca8331
Ibtid <ibtidrahman@iut-dhaka.edu>
2024-04-25 20:29:19 +0600

    remove margin right from button style

--- client/src/components/common/buttons/button.tsx
+++ client/src/components/common/buttons/button.tsx
@@ -28,13 +28,13 @@ interface IButtonProps {
     smMarginRight = false,
   }) => {
     
-    const marginRight = smMarginRight ? 'sm:mr-4' : '';
+    const marginRight = smMarginRight ? 'lg:mr-4' : 'sm:mr-0';
     const inlineDisplay = lgInline ? 'lg:inline' : 'hidden lg:inline';
   
     return (
       <button
         onClick={onClick}
-        className={`${bgColor} ${textColor} font-semibold border-2 ${borderColor} py-2 px-2 sm:px-3 rounded cursor-pointer transition duration-300 ease-in-out ${hoverBgColor} ${className} flex items-center`}
+        className={`${bgColor} ${textColor} font-semibold border-2 ${borderColor} py-2 px-2 sm:px-3 rounded cursor-pointer transition duration-300 ease-in-out ${hoverBgColor} ${className} flex items-center text-sm`}
       >
         <img
           src={iconSrc}
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -5,7 +5,6 @@ import Navbar from "../components/common/navbar/Navbar";
 import TasksPage from "../pages/Tasks";
 import AddTaskPage from "../pages/AddTask";
 import NotFoundPage from "../pages/NotFound";
-import EditTaskPage from "../pages/EditTask";
 
 const AppRoutes = () => {
   return (
@@ -14,7 +13,6 @@ const AppRoutes = () => {
         <Route index element={<Navigate to={UiPaths.TasksList} />} />
         <Route path={UiPaths.TasksList} element={<TasksPage />} />
         <Route path={UiPaths.AddTask} element={<AddTaskPage />} />
-        <Route path={UiPaths.EditTask} element={<EditTaskPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
